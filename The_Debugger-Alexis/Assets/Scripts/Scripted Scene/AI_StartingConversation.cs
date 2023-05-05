2be61f0ca9b36ab43fe8cdd04c3b81b2f2d2c583
Alien9119 <alexishdezh@gmail.com>
2023-05-05 10:05:16 -0600

    Version05/02/2023_10am

--- The_Debugger-Alexis/Assets/Scripts/Scripted Scene/AI_StartingConversation.cs
+++ The_Debugger-Alexis/Assets/Scripts/Scripted Scene/AI_StartingConversation.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class AI_StartingConversation : MonoBehaviour
 {
     private SpriteRenderer SpriteR;
+    private AudioSource AIAppear;
 
     void Start()
     {
         SpriteR = GetComponent<SpriteRenderer>();
         StartCoroutine(EventsTimeline());
-
+        AIAppear = GetComponent<AudioSource>();
     }
 
     IEnumerator EventsTimeline()
     {
         yield return new WaitForSeconds(8f);
         SpriteR.enabled = true;
+        AIAppear.Play();
         yield return new WaitForSeconds(44f);
         SpriteR.enabled = false;
     }
